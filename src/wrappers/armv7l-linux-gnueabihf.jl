b6ea1771375b6716eef4bdc04ef8da5f394098ed
jlbuild <juliabuildbot@gmail.com>
2020-12-18 11:48:55 +0000

    FLINT_jll build 200.700.0+0

--- src/wrappers/armv7l-linux-gnueabihf.jl
+++ src/wrappers/armv7l-linux-gnueabihf.jl
@@ -4,7 +4,7 @@ export libflint
 using GMP_jll
 using MPFR_jll
 JLLWrappers.@generate_wrapper_header("FLINT")
-JLLWrappers.@declare_library_product(libflint, "libflint.so.14")
+JLLWrappers.@declare_library_product(libflint, "libflint.so.15")
 function __init__()
     JLLWrappers.@generate_init_header(GMP_jll, MPFR_jll)
     JLLWrappers.@init_library_product(
--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -4,7 +4,7 @@ export libflint
 using GMP_jll
 using MPFR_jll
 JLLWrappers.@generate_wrapper_header("FLINT")
-JLLWrappers.@declare_library_product(libflint, "@rpath/libflint-14.dylib")
+JLLWrappers.@declare_library_product(libflint, "@rpath/libflint-15.dylib")
 function __init__()
     JLLWrappers.@generate_init_header(GMP_jll, MPFR_jll)
     JLLWrappers.@init_library_product(
