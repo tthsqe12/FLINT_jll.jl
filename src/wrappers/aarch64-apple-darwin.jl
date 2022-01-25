01441fe96fa20a4727528b8dd758ab4f4f004bf0
dan <tthsqe12@gmail.com>
2022-01-25 17:42:09 +0100

    FLINT_jll build 200.800.401+1

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -3,10 +3,11 @@ export libflint
 
 using GMP_jll
 using MPFR_jll
+using OpenBLAS32_jll
 JLLWrappers.@generate_wrapper_header("FLINT")
 JLLWrappers.@declare_library_product(libflint, "@rpath/libflint-16.dylib")
 function __init__()
-    JLLWrappers.@generate_init_header(GMP_jll, MPFR_jll)
+    JLLWrappers.@generate_init_header(GMP_jll, MPFR_jll, OpenBLAS32_jll)
     JLLWrappers.@init_library_product(
         libflint,
         "lib/libflint.dylib",
